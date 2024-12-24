b962f48b4ee11de7ea3400a80513453e5d1bb481
Henry <90460505+ofcourseitshenry@users.noreply.github.com>
2024-12-24 12:19:36 +0300

    Identity ve Authotentication(JWT) ayarlarımızı Program.cs içinde tamamladık.

--- Week14/24-12-2024/ECommerce/ECommerce.API/Program.cs
+++ Week14/24-12-2024/ECommerce/ECommerce.API/Program.cs
@@ -1,11 +1,17 @@
 using ECommerce.Business.Abstract;
 using ECommerce.Business.Concrete;
+using ECommerce.Business.Configuration;
 using ECommerce.Business.Mapping;
 using ECommerce.Data.Abstract;
 using ECommerce.Data.Concrete;
 using ECommerce.Data.Concrete.Contexts;
 using ECommerce.Data.Concrete.Repositories;
+using ECommerce.Entity.Concrete;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,6 +27,52 @@ builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<IProductService, ProductService>();
 
 builder.Services.AddAutoMapper(typeof(MappingProfile));
+//Identity ayarlarý 
+builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequiredLength = 8;
+
+    options.User.RequireUniqueEmail = true;
+    options.User.AllowedUserNameCharacters = "qwertyuopasdfghjklizxcvbnm1234567890-_.@";
+
+}).AddEntityFrameworkStores<ECommerceDbContext>().AddDefaultTokenProviders();
+//OptionsPattern kullanarak JwtConfig bilgilerini appsettings.json dosyasýndan okuyoruz.
+builder.Services.Configure<JwtConfig>(builder.Configuration.GetSection("JwtConfig"));
+var  jwtConfig = builder.Configuration.GetSection("JwtConfig").Get<JwtConfig>();
+
+
+
+
+//JWT(Authentication) ayarlarý
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience=true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer= jwtConfig?.Issuer,
+        ValidAudience = jwtConfig?.Audience,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtConfig.Secret))
+    };
+});
+
+
+
+
+
+
+
+
 
 var app = builder.Build();
 
